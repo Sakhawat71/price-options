b02ce2c03f0351ce7add033f4673f45df9df960d
sakhawat71 <sakhawat.showrav@gmail.com>
2023-11-03 16:52:22 +0600

    use deisy nav and stat

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import DaisyNav from './components/DaisyNav/DaisyNav';
+import Stat from './components/Stat/Stat';
 
 function App() {
 
   return (
     <>
-    
+        <DaisyNav></DaisyNav>
+        <Stat></Stat>
     </>
   )
 }
