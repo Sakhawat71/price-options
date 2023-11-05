d88540838962a2d3cf83ccd13e9270d4b8feeb8f
sakhawat71 <sakhawat.showrav@gmail.com>
2023-11-06 02:03:47 +0600

    explore react spinner 'Watch'

--- src/components/PhoneChart/PhoneChart.jsx
+++ src/components/PhoneChart/PhoneChart.jsx
@@ -1,36 +1,51 @@
 import axios from "axios";
 import { useState } from "react";
 import { Bar, BarChart, Tooltip, XAxis, YAxis } from "recharts";
+import { Watch } from "react-loader-spinner";
 
 const PhoneChart = () => {
 
-    const [phones,setPhones] = useState([]);
-    useState(()=>{
+    const [phones, setPhones] = useState([]);
+    useState(() => {
         axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
-        .then(data => {
-            const phoneData = data.data.data;
+            .then(data => {
+                const phoneData = data.data.data;
 
-            const phoneNewObj = phoneData.map(phone => {
-                const obj ={
-                    name: phone.phone_name,
-                    price: parseInt(phone.slug.split('-')[1])
-                }
-                return obj;
+                const phoneNewObj = phoneData.map(phone => {
+                    const obj = {
+                        name: phone.phone_name,
+                        price: parseInt(phone.slug.split('-')[1])
+                    }
+                    return obj;
+                })
+                setPhones(phoneNewObj);
             })
-            setPhones(phoneNewObj);
-        })
-    },[])
+    }, [])
 
     console.log(phones)
 
     return (
-        <div className="flex mx-auto">
-            <BarChart width={1250} height={250} data={phones}>
-                <XAxis dataKey={'name'}></XAxis>
-                <YAxis></YAxis>
-                <Tooltip></Tooltip>
-                <Bar dataKey={'price'} fill="blue"></Bar>
-            </BarChart>
+        <div className="mx-auto">
+            <div className="flex justify-center ">
+                <Watch
+                    height="80"
+                    width="80"
+                    radius="48"
+                    color="#4fa94d"
+                    ariaLabel="watch-loading"
+                    wrapperStyle={{}}
+                    wrapperClassName=""
+                    visible={true}
+                />
+            </div>
+            <div>
+                <BarChart width={1250} height={250} data={phones}>
+                    <XAxis dataKey={'name'}></XAxis>
+                    <YAxis></YAxis>
+                    <Tooltip></Tooltip>
+                    <Bar dataKey={'price'} fill="blue"></Bar>
+                </BarChart>
+            </div>
         </div>
     );
 };
